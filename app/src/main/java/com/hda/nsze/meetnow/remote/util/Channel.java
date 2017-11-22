940dd7008f98abd7ce68f0ffb5d7972c645a02f7
Chris <chris.weber@stud.tu-darmstadt.de>
2017-11-22 07:53:59 +0100

    Basic enumerations for easier communication

---
+++ app/src/main/java/com/hda/nsze/meetnow/remote/util/Channel.java
@@ -0,0 +1,9 @@
+package com.hda.nsze.meetnow.remote.util;
+
+/**
+ * Created by chris on 11/22/2017.
+ */
+
+public class Channel {
+    public static Channel DUMMY = new Channel();
+}
---
+++ app/src/main/java/com/hda/nsze/meetnow/remote/util/PIPChangeMode.java
@@ -0,0 +1,11 @@
+package com.hda.nsze.meetnow.remote.util;
+
+/**
+ * Created by chris on 11/22/2017.
+ */
+
+public enum PIPChangeMode {
+    ENLARGE,
+    SWAP,
+    COPY,
+}
